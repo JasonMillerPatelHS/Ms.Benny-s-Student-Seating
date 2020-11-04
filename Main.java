d2eb332f39b9e64f093f99ca8e6d487d7b719ade
JasonMillerPHS <jason.miller@patelhs.org>
2020-11-04 17:45:35 +0000

    Added a for loop to loop through the string array.

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ class Main {
   public static void main(String[] args) {
     int[] seatNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     String[] students = new String[15]; //adds 15 new elements with nothing assigned
-
+    for(int index = 0; index < students.length; index++)
+    {
+      
+    }
   }
 }
