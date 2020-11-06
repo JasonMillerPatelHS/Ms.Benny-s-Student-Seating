35a8dbde19c8d0a67c8308143f8eb35c34fb78a3
JasonMillerPHS <jason.miller@patelhs.org>
2020-11-06 14:54:28 +0000

    Imported scanner, added println question to help user know what to input.

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     int[] seatNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     String[] students = new String[15]; //adds 15 new elements with nothing assigned
     for(int index = 0; index < students.length; index++)
     {
-      
+      System.out.println("Who will sit at," + index + "?");
+
     }
   }
 }
