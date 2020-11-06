af38009b7c4ba9c302d2350c6f068566b67f2cde
JasonMillerPHS <jason.miller@patelhs.org>
2020-11-06 15:16:52 +0000

    Added the scanner, scan, which scans for users response of who will sit at which seat.

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
     int[] seatNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     String[] students = new String[15]; //adds 15 new elements with nothing assigned
-    for(int index = 0; index < students.length; index++)
+    int index = 0;
+    for(index = 0; index < students.length; index++)
     {
-      System.out.println("Who will sit at," + index + "?");
-
+      System.out.println("Who will sit at seat " + seatNumbers[index] + "? (Put Student's name)"); //prints out the question with seat number
+      students[index] = scan.next();
     }
+    
   }
 }
