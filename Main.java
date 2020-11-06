4ef9c92bce759255b6ec92034b7767f0a871f27a
JasonMillerPHS <jason.miller@patelhs.org>
2020-11-06 15:39:37 +0000

    Change scan.next to scan.nextLine so it does not only take in the first name of the student.

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ class Main {
     for(index = 0; index < students.length; index++)
     {
       System.out.println("Who will sit at seat " + seatNumbers[index] + "? (Put Student's name)"); //prints out the question with seat number
-      students[index] = scan.next();
+      students[index] = scan.nextLine();
     }
     System.out.println("\n Thank you for your input, here is the new seating chart:");
     index = 0;
